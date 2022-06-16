2c3f25d6013f0bc76165dd54e03226dca4245b50
Alexander Sterk <ajhsterk@gmail.com>
2022-06-16 21:16:22 +0200

    Updates

---
+++ precache-manifest.ffc9c6a7fbbfaafce17ce03e3606364f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f73b28e9fc29ee3cae0f9f78e715749",
+    "url": "/dalle-playground/index.html"
+  },
+  {
+    "revision": "12ee3577c3a6fb629174",
+    "url": "/dalle-playground/static/css/main.92e41723.chunk.css"
+  },
+  {
+    "revision": "1a1b5703de48b24ea525",
+    "url": "/dalle-playground/static/js/2.fe433896.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/dalle-playground/static/js/2.fe433896.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12ee3577c3a6fb629174",
+    "url": "/dalle-playground/static/js/main.017864b7.chunk.js"
+  },
+  {
+    "revision": "2287b2335ed2078787ec",
+    "url": "/dalle-playground/static/js/runtime-main.f1925457.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dalle-playground/precache-manifest.af2a6105a766b09a8bd1686884938a00.js"
+  "/dalle-playground/precache-manifest.ffc9c6a7fbbfaafce17ce03e3606364f.js"
 );
 
 self.addEventListener('message', (event) => {
